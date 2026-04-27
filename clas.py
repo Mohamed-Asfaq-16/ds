96c9bf6ee01271ccebcf548f3a06209e0663b6d1
Mohamed Asfaq <ashh.asfaq@gmail.com>
2026-04-28 02:09:17 +0530

    Add files via upload

--- clas.py
+++ clas.py
@@ -4,8 +4,20 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as py
 
 df=pd.read_csv("data.csv")
+sns.histplot(df["diagnosis"])
+plt.show()
+
+sns.scatterplot(x='radius_mean',y='texture_mean',hue='diagnosis',data=df)
+plt.show()
+
+sns.boxplot(x="radius_mean",y="texture_mean",data=df)
+plt.show()
+
 
 df = df.drop(['id','Unnamed: 32'], axis=1)
 print(df.isnull().sum())
@@ -15,6 +27,10 @@ df['diagnosis'] = df['diagnosis'].map({'M':1, 'B':0})
 
 x=df.drop("diagnosis",axis=1)
 y=df['diagnosis']
+
+corr=df.corr(numeric_only=True)
+sns.heatmap(corr,annot=True,cmap="coolwarm")
+plt.show()
 
 x_train,x_test,y_train,y_test=train_test_split(
     x,y,test_size=0.2,random_state=42
@@ -32,6 +48,7 @@ ans=model.predict(x_test)
 
 acc=accuracy_score(y_test,ans)
 print(acc)
+
 
 
 
